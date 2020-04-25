632728999b86908282dcb1f56903c8922e14f991
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2020-04-25 19:37:07 -0300

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 export const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  password: "password",
+  password: "",
   database: "taskapi",
   port: 5432,
 });
